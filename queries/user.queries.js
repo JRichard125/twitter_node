f883baa1b50b262446a5481890584ff1d2579313
joakim richard <joakim.richard49@hotmail.com>
2023-02-28 10:10:07 +0100

    feat: Configuration de passport et localstrategie

--- queries/user.queries.js
+++ queries/user.queries.js
@@ -20,3 +20,11 @@ exports.createNewUser = async (user) => {
         throw error
    }
 }
+
+exports.findUserByEmail =(email) => {
+    return User.findOne({'local.email' : email}).exec();
+}
+
+exports.findUserById = (id) => {
+    return User.findById(id).exec()
+}
