4f1bcd2e87d615103c863835106bf12635bcdd77
joakim richard <joakim.richard49@hotmail.com>
2023-03-16 11:02:13 +0100

    feat: Comment route added

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,14 @@ const router = require('express').Router();
 const tweetRoute = require('./tweet.routes');
 const userRoute = require('./user.routes');
 const authRoute = require('./auth.routes')
+const commentRoute = require('./comment.routes')
 const { tweetList } = require('../controllers/tweet.controllers');
 const { ensureAuthenticated } = require('../config/security.config');
 
 router.use('/tweet', ensureAuthenticated, tweetRoute);
 router.use('/user', userRoute);
 router.use('/auth', authRoute)
+router.use('/comment', ensureAuthenticated, commentRoute);
 
 router.get('/', tweetList);
 
